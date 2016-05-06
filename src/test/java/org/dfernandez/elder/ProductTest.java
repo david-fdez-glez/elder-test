444c9133f27f7b3739b7f36ff5339c112021877a
David Fernández González <fernandezgonzalez.d@gmail.com>
2016-05-06 19:52:01 +0100

    Product Object

---
+++ src/test/java/org/dfernandez/elder/ProductTest.java
@@ -0,0 +1,49 @@
+package org.dfernandez.elder;
+
+import static org.junit.Assert.*;
+
+import org.dfernandez.elder.model.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class ProductTest {
+    
+    Product productTest;
+
+
+    @Before
+    public void init() {
+    }
+    
+    
+    @Test
+    public void createProductTest() {
+        productTest = new Product("testProduct");
+        assertTrue("testProduct".equals(productTest.getName()));
+    }
+
+    @Test
+    public void getProduct1Item() {
+        productTest = new Product("1");
+        productTest.setQuantity(1);
+        assertEquals(1,productTest.getQuantity());
+    }
+
+    @Test
+    public void getProductPrice() {
+        productTest = new Product("1");
+        productTest.setPrice(new BigDecimal(20));
+        assertEquals(new BigDecimal(20),productTest.getPrice());
+    }
+
+
+    @Test
+    public void getProductPriceFiftyPence() {
+        productTest = new Product("1");
+        productTest.setPrice(new BigDecimal(0.5));
+        assertEquals(new BigDecimal(0.5),productTest.getPrice());
+    }
+
+}
