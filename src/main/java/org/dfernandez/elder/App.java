3246d47abc5638204147c0ae1ee8d0f3cc200534
David Fernández González <fernandezgonzalez.d@gmail.com>
2016-05-06 15:32:40 +0100

    Initial Structure

---
+++ src/main/java/org/dfernandez/elder/App.java
@@ -0,0 +1,13 @@
+package org.dfernandez.elder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
